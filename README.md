626d3719dfb9e0cc9c08885602145232bef62223
bilalasyikin <bilalzaenal@gmail.com>
2024-10-05 16:11:16 +0700

    tf init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Getting Started with Terraform on Google Cloud Platform
+
+This repository demonstrates how to build infrastructure on Google Cloud Platform (GCP) using [Terraform](https://developer.hashicorp.com/terraform). The setup guides you through the process of provisioning resources in GCP using Terraform configurations.
+https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build
---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "linen-badge-437708"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
